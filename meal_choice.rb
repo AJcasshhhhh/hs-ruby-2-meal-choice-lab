aa1c1f32d3dd238473880026ec5caa804e51be4b
Daniel Fenjves <dfenjves@users.noreply.github.com>
2014-10-02 10:30:26 -0400

    Update meal_choice.rb

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,10 @@
-# Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
+# Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
 def snacks(meal_choice="Cheetos")
   "Any time, is the right time for #{meal_choice}!" 
 end
+
+# Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
+
 
 
 # Call the methods with puts and your own arguments here. Like this:
